9b635c4e19afb77ac3ac0852d0d76a2b39b1cca0
hifdzimaulana34 <hifdzimaulana.19934@gmail.com>
2021-10-16 22:36:09 +0700

    menambahkan fungsi hapus data

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -3,18 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('STUDENT DATA') }}</div>
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">{{ __('STUDENT DATA') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    <a href="/students/create" class="btn btn-primary">Add Data</a> <br><br>
+                    <a href="/students/create" class="btn btn-primary">Add Data</a> 
+                    <br><br>
 
                     <table class="table table-responsive table-striped">
                         <thead>
@@ -22,7 +23,7 @@
                                 <th>NIM</th>
                                 <th>Name</th>
                                 <th>Class</th>
-                                <th>Action</th>
+                                <th>Action</th> 
                             </tr>
                         </thead>
                         <tbody>
@@ -31,7 +32,16 @@
                                 <td>{{ $s->nim }}</td>
                                 <td>{{ $s->name }}</td>
                                 <td>{{ $s->class }}</td>
-                                <td><a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a></td>
+                                <td>
+                                    <form action="/students/{{$s->id}}" method="post">
+                                        <a href="/students/{{$s->id}}/edit" class="btn btn-warning">Edit</a>
+
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" name="delete" class="btn btn-danger">Delete</button>
+                                        
+                                    </form>
+                                </td>
                             </tr>
                             @endforeach
                         </tbody>
